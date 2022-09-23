92f69dcc43f92c07fabbe0de1cb48c883ca0d19a
Lizosoma <elshinaliza@yandex.ru>
2022-09-24 00:11:42 +0300

    Решила задачи про массивы.

---
+++ src/ArraysTasks.java
@@ -0,0 +1,71 @@
+import java.util.Arrays;
+
+
+public class ArraysTasks {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(even(5)));
+        System.out.println(allSimilar(new int[]{9, 9, 9}));
+        System.out.println(hasSimilar(new int[]{1, 6, 3, 9, 4}));
+        System.out.println(mean(new int[]{9, 8, 7, 6, 5}));
+        int[] array = {10, 20, 30, 40};
+        shift(array);
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(copyShift(array)));
+    }
+
+    private static int[] even(int n) {
+        int[] arrayOfInts = new int[n];
+        for (int i = 1; i <= n; i++) {
+            arrayOfInts[i - 1] = i * 2;
+        }
+        return arrayOfInts;
+    }
+
+    private static boolean allSimilar(int[] arrayOfInts) {
+        if (arrayOfInts.length == 1) {
+            return true;
+        }
+        for (int i = 1; i < arrayOfInts.length; i++) {
+            if (arrayOfInts[i - 1] != arrayOfInts[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean hasSimilar(int[] arrayOfInts) {
+        Arrays.sort(arrayOfInts);
+        for (int i = 1; i < arrayOfInts.length; i++) {
+            if (arrayOfInts[i - 1] == arrayOfInts[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static double mean(int[] arrayOfInts) {
+        int sum = 0;
+        for (int i = 0; i < arrayOfInts.length; i++) {
+            sum += arrayOfInts[i];
+        }
+        return sum / arrayOfInts.length;
+    }
+
+    public static void shift(int[] arrayOfInts) {
+        int last = arrayOfInts[arrayOfInts.length - 1];
+        for (int i = arrayOfInts.length - 2; i >= 0; i--) {
+            arrayOfInts[i + 1] = arrayOfInts[i];
+        }
+        arrayOfInts[0] = last;
+    }
+
+    public static int[] copyShift(int[] arrayOfInts) {
+        int[] arrayOfIntsNew = new int[arrayOfInts.length];
+        int last = arrayOfInts[arrayOfInts.length - 1];
+        arrayOfIntsNew[0] = last;
+        for (int i = arrayOfInts.length - 2; i >= 0; i--) {
+            arrayOfIntsNew[i + 1] = arrayOfInts[i];
+        }
+        return arrayOfIntsNew;
+    }
+}
